858e7bef3a3f428e0e46400d1d97f427b584ade6
Hasan <hasanozzamankhan96@gmail.com>
2021-09-12 23:25:47 +0600

    HRM System

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('master')
+
+@section('title', 'OHCL')
+
+@section('content')
+
+<div class="container">
+
+  <div class="mt-5">
+    <!-- <div class="text-center my-3">
+      <img src="{{asset('image/ohcl.png')}}" alt="">
+    </div> -->
+    <h1 class="text-center">WELCOME TO</h1>
+    <h4 class="text-center">HRM System</h4>
+    <h4 class="text-center">of</h4>
+    <h2 class="text-center">Company Name</h2>
+  </div>
+</div>
+@endsection
